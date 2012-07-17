e3bd9f1a0ac93f83a9471cee0a944f86c4e98086
Ben Stolovitz <ben@stolovitz.com>
2012-07-17 13:34:57 -0400

    Correctly parses.
    Added cosmetics.

--- parser.rb
+++ parser.rb
@@ -5,12 +5,6 @@ require 'pp'
 
 # Ruby On Rails: http://bit.ly/Nypi1I
 class Hash
-  # Returns a hash that represents the difference between two hashes.
-  #
-  #   {1 => 2}.diff(1 => 2)         # => {}
-  #   {1 => 2}.diff(1 => 3)         # => {1 => 2}
-  #   {}.diff(1 => 2)               # => {1 => 2}
-  #   {1 => 2, 3 => 4}.diff(1 => 2) # => {3 => 4}
   def diff(other)
 	dup.
 	  delete_if { |k, v| other[k] == v }.
@@ -25,8 +19,9 @@ def parse(file)
 	data = arr_of_arrs.map {|row| row.map {|cell| cell.to_s } }
 	
 	max_length = arr_of_arrs.max_by(&:length).length
+	headers.pop
 	
-	headers_to_add = max_length - headers.length
+	headers_to_add = max_length - 1 - headers.length
 	
 	headers_to_add.times do |i|
 		headers.push("Ship #{i+1}")
@@ -38,52 +33,78 @@ def parse(file)
 	return parsed_csv
 end
 
-def filter!(file)
-	file.each_index do|index|
-		row = file[index]
-		last_row = file[index-1]
+def filter!(data)
+	data.each_index do|index|
+		row = data[index]
+		last_row = data[index-1]
 		
 		continue if(last_row == nil)
 		
 		differences = row.diff(last_row)
 		
 		# Only check for differences in important variables
-		differences.delete("Timestamp").delete("Width").delete("MouseX").delete("Ships")
+		differences.delete("Timestamp")
+		differences.delete("Width")
+		differences.delete("MouseX")
+		differences.delete("to_delete")
 		
-		differences.keep_if {|k, v| last_row[k] == nil }
+		differences.keep_if {|k, v| last_row[k] == nil || row[k] == nil || k == "Score" }
 		
-		puts index if(differences.length > 0)
+		puts "#{index + 2} #{differences.inspect}" if(differences.length > 0 && $verbose)
+		
+		row["to_delete"] = (differences.length == 0)
 	end
 	
-	file.replace(filtered_file)
+	data.delete_if {|row| row["to_delete"] == true }
 end
 
-
-if(ARGV[0] == nil)
-	ARGV[0] = '/Users/citelao/Desktop/navy logs - start/navy_activity-1340907930347.log'
+def save(data, filename)
+	filename = filename + "1" if (File.exist?(filename))
+	
+	CSV.open(filename, "wb") do |csv|
+		csv << data[0].keys.slice(0, data[0].keys.length-1)
+				
+		data.each do|row|
+			csv << row.values.slice(0, row.values.length-1)
+		end
+	end
 end
 
-ARGV.each do|file|
-	puts "Using #{file} \r\n"
-	puts
-	
-	puts "Parsing..."
-	data = parse(file)
-	puts "Done!"
-	puts
-	
-	puts "Filtering..."
-	filter!(data)
-	puts "Done!"
-	puts
-	
-	puts "Calculating accuracy..."
-	puts "Not Implemented."
-	puts "Done!"
-	puts
-	
-	puts "Saving..."
-	puts "Not Implemented."
-	puts "Done!"
-	puts
+ARGV.each do|arg|
+	case
+	when (arg == "--verbose" || arg == "-v")
+		$verbose = true
+	when (arg == "--help" || arg == "-h")
+		puts "Parser for Navy"
+		puts "Parser by Ben Stolovitz <ben@stolovitz.com>"
+		puts "Navy by Ben Davison"
+		puts
+		puts "Usage: `./parser.rb [args] file [file...]`"
+		puts "--verbose, -v: verbose messaging"
+		puts "--help, -h: this help text"
+	else
+		puts "Using #{arg} \r\n"
+		puts
+		
+		puts "Parsing..."
+		data = parse(arg)
+		puts "Done!"
+		puts
+		
+		puts "Filtering..."
+		filter!(data)
+		puts "Done!"
+		puts
+						
+		puts "Calculating accuracy..."
+		puts "Not Implemented."
+		puts "Done!"
+		puts
+		
+		puts "Saving..."
+		save(data, "#{arg[0...-4]}.treated.log")
+		puts "Done!"
+		puts
+
+	end
 end
