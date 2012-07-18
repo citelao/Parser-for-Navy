930ef3b6d78e6cc463f412cd8448637694f14c3d
Ben Stolovitz <ben@stolovitz.com>
2012-07-18 15:07:36 -0400

    Refactoring and beautification and featurizing

--- parser.rb
+++ parser.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
-require 'csv'
-require 'pp'
 require 'clamp'
+require 'csv'
+require 'highline'
+require 'pp'
 require 'progress_bar'
 
 # Ruby On Rails: http://bit.ly/Nypi1I
+# Adds a difference comparison for parsing.
 class Hash
   def diff(other)
 	dup.
@@ -14,51 +16,51 @@ class Hash
   end
 end
 
-class NavyParser < Clamp::Command 
-	option ["-v", "--verbose"], :flag, "be chatty"
-	
-	parameter "FILE ...", "input files / folders"
-	
-	def initialize(unused, also_unused)
-		@parsable_files = []
-		@bar_flags = [:percentage, :bar, :eta]
-	end
-	
-	def file_list=(file)
-		file.map do|item|
-			Dir.glob("**/*.log") do|file|
-				@parsable_files.push(file)
-			end if File.directory?(item)
-			
-			@parsable_files.push(item) unless File.directory?(item)
-		end
-	end
-	
-	def csv_parse(file)		
+class Extrapolated_CSV
+	# Reads CSV data from a filename into a key-value hash
+	def initialize(file, debug = false)
 		arr_of_arrs = CSV.read(file)
 		
 		headers = arr_of_arrs.shift.map {|head| head.to_s }
 		data = arr_of_arrs.map {|row| row.map {|cell| cell.to_s } }
 		
 		max_length = arr_of_arrs.max_by(&:length).length
-		headers.delete("Ships")
+		headers.delete("Ships")	# Remove the ambiguous header "Ships"
+								# It will become "Ship 1"
 		
 		headers_to_add = max_length - headers.length 	
-		headers_to_add.times do |i|
-			headers.push("Ship #{i+1}")
-		end
+		headers_to_add.times {|i| headers.push("Ship #{i+1}") } \
+			unless headers_to_add < 1
+		
+		# Add extrapolated headers
+		headers.push("Shots")
+		headers.push("Hits")
+		headers.push("Misses")
 		
 		# Shamelessly: http://bit.ly/Q4kGVn
-		return arr_of_arrs.map \
-		 {|row| Hash[*headers.zip(row).flatten]}
+		# Interpolate the headers with the data, then flatten
+		# Because Hash accepts [key, value, key, value, ...]
+		@csv = arr_of_arrs.map {|row| Hash[*headers.zip(row).flatten]}
+		@debug = debug
+		return @csv
 	end
 	
-	def csv_filter(exploded_csv)
-		exploded_csv.each_index do|index|
-			row = exploded_csv[index]
-			last_row = exploded_csv[index-1]
+	def [](x)
+		@csv[x]
+	end
+	
+	def each(&block)
+		@csv.each(&block)
+	end
+	
+	def filter
+		@csv.each_index do|index|
+			row = @csv[index]
+			last_row = @csv[index-1]
 			
-			continue if(last_row == nil)
+			# Arrays *love* wrapping around at the beginning
+			# Array[-1] returns the last element-- that's not good
+			next if(last_row["Timestamp"].to_i > row["Timestamp"].to_i)
 			
 			differences = row.diff(last_row)
 			
@@ -72,42 +74,139 @@ class NavyParser < Clamp::Command
 				last_row[k] == nil || row[k] == nil || k == "Score" 
 			end
 			
-			puts "#{index + 2} #{differences.inspect}" \
-			 if(differences.length > 0 && verbose?)
+			# List differences by line in --debug mode
+			puts "ln #{index + 2}: #{differences.inspect}" \
+			 if(differences.length > 0 && @debug)
 			
+			# Add deletion key to each element. They are deleted later.
 			row["to_delete"] = (differences.length == 0)
 		end
 		
-		exploded_csv.delete_if {|row| row["to_delete"] == true }
-		exploded_csv.each{|row| row.delete("to_delete")}
-		
-		return exploded_csv
+		@csv.delete_if {|row| row["to_delete"] == true }
+		@csv.each{|row| row.delete("to_delete")}
+	end
+	
+	def extrapolate
+		@csv.each_index do|index|
+			row = @csv[index]
+			last_row = @csv[index-1]
+			
+			differences = row.diff(last_row)
+			
+			row["Score"], row["Misses"], row["Hits"] =  "0" \
+				if last_row["Timestamp"].to_i > row["Timestamp"].to_i
+			
+			if differences.has_key?("Score") and \
+				last_row["Timestamp"].to_i < row["Timestamp"].to_i
+				score_dif = row["Score"].to_i - last_row["Score"].to_i
+				case
+				when score_dif < 0 # Lost points - miss or ship left screen
+					puts "Missed. #{score_dif} #{row['Timestamp']}" \
+						if @debug
+					row["Misses"] = (last_row["Misses"].to_i + 1).to_s;
+				when score_dif  > 0 # Gained points - hit
+#					puts "Hit. #{row['Timestamp']}" if verbose?
+					row["Hits"] = (last_row["Hits"].to_i + 1).to_s;
+				else # da-whuh?
+					puts "Um. This shouldn't happen. #{score_dif}"
+					puts "Something's broken on Ruby-on-Rail's side. (ln 9)"
+				end
+				row["Shots"] = (last_row["Shots"].to_i + 1).to_s;
+			else 
+				row["Shots"], row["Misses"], row["Hits"] =\
+					*last_row.values_at("Shots", "Misses", "Hits")
+			end
+		end
+	end
+end
+
+class Navy_Parser < Clamp::Command 
+	option ["-v", "--verbose"], :flag, "be chatty"
+	option ["-d", "--debug"], :flag, "moar output? OK."	
+	
+	parameter "FILE ...", "input files / folders"
+	
+	def initialize(unused, also_unused)
+		@parsable_files = []
+		@bar_flags = [:percentage, :bar, :eta]
+		@steps = 4
+	end
+	
+	def file_list=(file)
+		file.map do|item|
+			Dir.glob("**/*.log") do|file|
+				@parsable_files.push(file)
+			end if File.directory?(item)
+			
+			@parsable_files.push(item) unless File.directory?(item)
+		end
 	end
 	
 	def execute
-		puts "Parsing..."
-		parse_bar = ProgressBar.new(@parsable_files.length, *@bar_flags)
+		terminal_width = HighLine::SystemExtensions.terminal_size[0]
 		
-		exploded_csv = Hash.new
+		puts "Parsing #{@parsable_files.length} files."
+		
+		if verbose?
+			print "Parsing..."
+			print " " * (terminal_width-10-11)
+			print "Step 1 of #{@steps} \r"
+			
+			parse_bar = ProgressBar.new(@parsable_files.length, *@bar_flags)
+		else
+			bar = ProgressBar.new(@parsable_files.length * 4, *@bar_flags)
+		end
+		
+		exploded_csvs = Hash.new
+		
 		@parsable_files.map do|file| 
-			parse_bar.increment!
-			exploded_csv[file] = csv_parse(file) 
+			parse_bar.increment! if verbose?
+			bar.increment! unless verbose?
+			
+			exploded_csvs[file] = Extrapolated_CSV.new(file, debug?) 
 		end
 		
-		puts "Filtering..."
-		filter_bar = ProgressBar.new(exploded_csv.length, *@bar_flags)
-		
-		filtered_csv = Hash.new
-		exploded_csv.each do|key, data|
-			filter_bar.increment!
-			filtered_csv[key] = csv_filter(data)
+		if verbose?
+			print "Filtering..."
+			print " " * (terminal_width-12-11)
+			print "Step 2 of #{@steps} \r"
+			
+			filter_bar = ProgressBar.new(exploded_csvs.length, *@bar_flags)
 		end
 		
-		puts "Saving..."
-		saving_bar = ProgressBar.new(@parsable_files.length, *@bar_flags)
+		exploded_csvs.each do|file, csv|
+			filter_bar.increment! if verbose?
+			bar.increment! unless verbose?
+			
+			csv.filter
+		end
 		
-		filtered_csv.each do|file, data|
-			saving_bar.increment!
+		if verbose?
+			print "Extrapolating..."
+			print " " * (terminal_width-16-11)
+			print "Step 3 of #{@steps} \r"
+		
+			extrapolate_bar = ProgressBar.new(exploded_csvs.length, *@bar_flags)
+		end
+		
+		exploded_csvs.each do|file, csv|
+			extrapolate_bar.increment! if verbose?
+			bar.increment! unless verbose?
+			
+			csv.extrapolate
+		end
+		
+		if verbose?
+			print "Saving..."
+			print " " * (terminal_width-9-11)
+			print "Step 4 of #{@steps} \r"
+			
+			saving_bar = ProgressBar.new(exploded_csvs.length, *@bar_flags)
+		end
+		
+		exploded_csvs.each do|file, data|
+			saving_bar.increment! if verbose?
+			bar.increment! unless verbose?
 			
 			filename = "#{file[0...-4]}.treated.csv"			
 			i = 2
@@ -127,4 +226,4 @@ class NavyParser < Clamp::Command
 	end
 end
 
-NavyParser.run
+Navy_Parser.run
